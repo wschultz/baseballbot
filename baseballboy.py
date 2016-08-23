e0ace478634640c55512ae3be69116b2227a2501
wschultz <wschultz@twitter.com>
2016-08-23 14:47:07 -0700

    added in a test mode

--- baseballboy.py
+++ baseballboy.py
@@ -6,6 +6,7 @@
     with any changes. 
 """
 
+testmode = True
 
 import private.credentials
 
@@ -111,40 +112,55 @@ def do_the_things():
 
     if not game_data and not returned_no_game:
       returned_no_game = True
-      #twitter.tweet("The %s don't have a game scheduled today. Rest well guys!" % team_hashtag)
-      print("The %s don't have a game scheduled today. Rest well guys!" % team_hashtag)
+      if testmode:
+        print("The %s don't have a game scheduled today. Rest well guys!" % team_hashtag)
+      else:
+        twitter.tweet("The %s don't have a game scheduled today. Rest well guys!" % team_hashtag)
 
     if game_data and not returned_game_time:
       returned_game_time = True
-      #twitter.tweet(("The %s are playing against the %s today, first pitch is at " + pacific_time.strftime("%-I:%M%p %Z") + " at %s") % (team_hashtag, opponent, venue))
-      print(("The %s are playing against the %s today, first pitch is at " + pacific_time.strftime("%-I:%M%p %Z") + " at %s") % (team_hashtag, opponent, venue))
+      if testmode:
+        print(("The %s are playing against the %s today, first pitch is at " + pacific_time.strftime("%-I:%M%p %Z") + " at %s") % (team_hashtag, opponent, venue))
+      else:
+        twitter.tweet(("The %s are playing against the %s today, first pitch is at " + pacific_time.strftime("%-I:%M%p %Z") + " at %s") % (team_hashtag, opponent, venue))
 
     if "Warmup" in game_data["status"] and not returned_game_soon:
       returned_game_soon = True
-      #twitter.tweet(("The %s are playing against the %s in a moment, first pitch is at " + pacific_time.strftime("%-I:%M%p %Z") + " at %s") % (team_hashtag, opponent, venue))
-      print(("The %s are playing against the %s in a moment, first pitch is at " + pacific_time.strftime("%-I:%M%p %Z") + " at %s") % (team_hashtag, opponent, venue))
+      if testmode:
+        print(("The %s are playing against the %s in a moment, first pitch is at " + pacific_time.strftime("%-I:%M%p %Z") + " at %s") % (team_hashtag, opponent, venue))
+      else:
+        twitter.tweet(("The %s are playing against the %s in a moment, first pitch is at " + pacific_time.strftime("%-I:%M%p %Z") + " at %s") % (team_hashtag, opponent, venue))
 
     if "In Progress" in game_data["status"]:
       if int(our_score) > int(their_score):
-        print("The %s are winning against the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
-        #twitter.tweet("The %s are winning against the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
+        if testmode:
+          print("The %s are winning against the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
+        else:
+          twitter.tweet("The %s are winning against the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
       elif int(our_score) < int(their_score):
-        print("The %s are losing against the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
-        #twitter.tweet("The %s are losing against the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
+        if testmode:
+          print("The %s are losing against the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
+        else:
+          twitter.tweet("The %s are losing against the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
       elif int(our_score) == int(their_score):
-        print("The %s are tied with the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
-        #twitter.tweet("The %s are tied with the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
+        if testmode:
+          print("The %s are tied with the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
+        else:
+          twitter.tweet("The %s are tied with the %s, the score is currently %s-%s" % (team_hashtag, opponent, our_score, their_score))
 
     if "Final" in game_data["status"]:
       returned_game_final = True
       if our_score > their_score:
-        print("The %s beat the %s today at %s with a score of %s-%s" % (team_hashtag, opponent, venue, our_score, their_score))
-        #twitter.tweet("The %s beat the %s today at %s with a score of %s-%s" % (team_hashtag, opponent, venue, our_score, their_score))
+        if testmode:
+          print("The %s beat the %s today at %s with a score of %s-%s" % (team_hashtag, opponent, venue, our_score, their_score))
+        else:
+          twitter.tweet("The %s beat the %s today at %s with a score of %s-%s" % (team_hashtag, opponent, venue, our_score, their_score))
       else:
-        print("The %s lost against the %s today at %s with a score of %s-%s. Get 'em tomorrow!" % (team_hashtag, opponent, venue, our_score, their_score))
-        #twitter.tweet("The %s lost against the %s today at %s with a score of %s-%s. Get 'em tomorrow!" % (team_hashtag, opponent, venue, our_score, their_score))
+        if testmode:
+          print("The %s lost against the %s today at %s with a score of %s-%s. Get 'em tomorrow!" % (team_hashtag, opponent, venue, our_score, their_score))
+        else:
+          twitter.tweet("The %s lost against the %s today at %s with a score of %s-%s. Get 'em tomorrow!" % (team_hashtag, opponent, venue, our_score, their_score))
 
-    #print("reached the end of the loop")
     time.sleep(5)
 
 
@@ -173,19 +189,19 @@ def set_vars(game_data):
 if __name__ == '__main__':
   print "Starting..."
 
-  do_the_things()
-  # These 4 lines are required
-  sched = BackgroundScheduler()
-  #sched.add_job(do_the_things, 'cron', hour=8, minute=0, end_date=last_day)
-  #
-  #sched.start()
+  if testmode:
+    do_the_things()
+  else:
+    sched = BackgroundScheduler()
+    sched.add_job(do_the_things, 'cron', hour=8, minute=0, end_date=last_day)
+
+    sched.start()
 
   try:
-    # This is here to simulate application activity (which keeps the main thread alive).
+    # Poorman daemon
     while True:
-        time.sleep(2)
+      time.sleep(2)
   except (KeyboardInterrupt, SystemExit):
-    # Not strictly necessary if daemonic mode is enabled but should be done if possible
     sched.shutdown()
 
 
